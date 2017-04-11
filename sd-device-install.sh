42d3631a97ba596bc6fb3f3df168ebf48ffeb2c3
Jacob Sieberg <jsieberg123@gmail.com>
2017-04-10 23:14:44 -0500

    Added cron

--- sd-device-install.sh
+++ sd-device-install.sh
@@ -2,13 +2,8 @@
 
 # Install using command "wget -O - https://apps.jacobsieberg.info/apps/sd-device-install.sh | sudo  bash"
 
-#apt-get update
-#apt-get install npm -y
 V=$(curl https://apps.jacobsieberg.info/apps/sd-device.version)
 wget  https://apps.jacobsieberg.info/apps/sd-device_1.$V-1.deb -O sd-device.deb --no-cache
 dpkg -i sd-device.deb
 apt-get install -f -y
-#echo "00 * * * * wget -O - https://apps.jacobsieberg.info/apps/sd-device-install.sh | sudo  bash" >> mycron
-#crontab mycron
-#rm mycron
-#rm sd-device.dep
+rm sd-device.dep
