1a030feacd146395346a5346d78fa45198acc850
Jacob Sieberg <jsieberg123@gmail.com>
2017-04-10 22:27:25 -0500

    Removed the SHIT

--- Jenkinsfile
+++ Jenkinsfile
@@ -7,6 +7,7 @@ pipeline {
                 sh 'echo $BUILD_NUMBER > /apps/sd-device.version'
                 sh 'packageFiles/package.sh $BUILD_NUMBER'
                 sh 'cp -f bin/*.deb /apps/'
+                sh 'cp -f sd-device-install.sh /apps/sd-device-install.sh'
             }
         }
     }
--- sd-device-install.sh
+++ sd-device-install.sh
@@ -4,16 +4,6 @@ apt-get update
 apt-get install npm -y
 npm install n -g
 n stable
-wget --user getapp --password SeniorDesign2017 https://apps.jacobsieberg.info/sd-device.hash -O sd-device.hash --no-cache
-wget --user getapp --password SeniorDesign2017 https://apps.jacobsieberg.info/sd-device.zip -O sd-device.zip --no-cache
-mkdir sd-device
-unzip sd-device.zip -d sd-device
-rm sd-device.zip
-cd sd-device
-rm node.json -f
-npm install
-chmod +x sd-device.sh
-chmod +x start.sh
-cp sd-device.service /lib/systemd/system/sd-device.service
-systemctl enable sd-device.service
-systemctl start sd-device.service
+V=$(curl https://apps.jacobsieberg.info/apps/sd-device.version)
+wget  https://apps.jacobsieberg.info/apps/sd-device_1.$V-1.deb -O sd-device.deb --no-cache
+dpkg -i sd-device.deb
