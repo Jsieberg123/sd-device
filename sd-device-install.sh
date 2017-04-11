c2a6904409b8a0f62aa70d9c9a593cd8ee556d8a
Jacob Sieberg <jsieberg123@gmail.com>
2017-04-10 23:17:45 -0500

    fix package name

--- sd-device-install.sh
+++ sd-device-install.sh
@@ -6,4 +6,4 @@ V=$(curl https://apps.jacobsieberg.info/apps/sd-device.version)
 wget  https://apps.jacobsieberg.info/apps/sd-device_1.$V-1.deb -O sd-device.deb --no-cache
 dpkg -i sd-device.deb
 apt-get install -f -y
-rm sd-device.dep
+rm sd-device.deb
