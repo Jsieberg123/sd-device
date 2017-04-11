be77461de6bce85f91e964f89d43f722d5cb3317
Jacob Sieberg <jsieberg123@gmail.com>
2017-04-10 22:58:56 -0500

    try this

--- sd-device-install.sh
+++ sd-device-install.sh
@@ -2,10 +2,13 @@
 
 # Install using command "wget -O - https://apps.jacobsieberg.info/apps/sd-device-install.sh | sudo  bash"
 
-apt-get update
-apt-get install npm -y
-npm install n -g
-n stable
+#apt-get update
+#apt-get install npm -y
 V=$(curl https://apps.jacobsieberg.info/apps/sd-device.version)
 wget  https://apps.jacobsieberg.info/apps/sd-device_1.$V-1.deb -O sd-device.deb --no-cache
 dpkg -i sd-device.deb
+apt-get install -f
+#echo "00 * * * * wget -O - https://apps.jacobsieberg.info/apps/sd-device-install.sh | sudo  bash" >> mycron
+#crontab mycron
+#rm mycron
+#rm sd-device.dep
